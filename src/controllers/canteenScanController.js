2ea9e2a2120f5e3fd1f8ba6983deb2d861fcf520
rxmy43 <ramyabyyu43@gmail.com>
2024-06-16 22:53:41 +0700

    Statistics for canteen scans

--- src/controllers/canteenScanController.js
+++ src/controllers/canteenScanController.js
@@ -2,6 +2,7 @@ const db = require('../config/db');
 const { httpResponse, serverErrorResponse } = require('../utils/httpResponse');
 const { validationResult } = require('express-validator');
 const httpStatus = require('http-status');
+const moment = require('moment-timezone');
 
 const inputScan = async (req, res) => {
     try {
@@ -170,8 +171,144 @@ const resetLastScanningQR = async (req, res) => {
     }
 };
 
+const getStatistics = async (req, res) => {
+    try {
+        const { filter_by } = req.query;
+
+        let sql;
+        let periods = [];
+        let queryParams = [];
+
+        if (filter_by === 'month') {
+            sql = `
+                SELECT 
+                    DATE_FORMAT(CONVERT_TZ(created_at, '+00:00', 'Asia/Jakarta'), '%Y-%m') AS period,
+                    COUNT(*) AS scan_count
+                FROM 
+                    canteen_scans
+                GROUP BY 
+                    period
+                ORDER BY 
+                    period;
+            `;
+            const startDate = moment().startOf('year');
+            const endDate = moment().endOf('year');
+            while (startDate.isBefore(endDate)) {
+                periods.push(startDate.format('YYYY-MM'));
+                startDate.add(1, 'month');
+            }
+        } else if (filter_by === 'day') {
+            const startDate = moment().startOf('isoWeek'); // Start of the current week (Monday)
+            const endDate = moment().endOf('isoWeek'); // End of the current week (Sunday)
+
+            sql = `
+                SELECT 
+                    DATE_FORMAT(CONVERT_TZ(created_at, '+00:00', 'Asia/Jakarta'), '%Y-%m-%d') AS period,
+                    COUNT(*) AS scan_count
+                FROM 
+                    canteen_scans
+                WHERE 
+                    created_at BETWEEN ? AND ?
+                GROUP BY 
+                    period
+                ORDER BY 
+                    period;
+            `;
+
+            queryParams = [
+                startDate.format('YYYY-MM-DD'),
+                endDate.format('YYYY-MM-DD'),
+            ];
+
+            while (startDate.isBefore(endDate)) {
+                periods.push(startDate.format('YYYY-MM-DD'));
+                startDate.add(1, 'day');
+            }
+        } else if (filter_by === 'week') {
+            const month = moment().format('YYYY-MM');
+            sql = `
+                SELECT 
+                    CONCAT(YEAR(CONVERT_TZ(created_at, '+00:00', 'Asia/Jakarta')), '-W', WEEK(CONVERT_TZ(created_at, '+00:00', 'Asia/Jakarta'))) AS period,
+                    COUNT(*) AS scan_count
+                FROM 
+                    canteen_scans
+                GROUP BY 
+                    period
+                ORDER BY 
+                    period;
+            `;
+
+            const startDate = moment(month, 'YYYY-MM').startOf('month');
+            const endDate = moment(month, 'YYYY-MM').endOf('month');
+            let weekNums = 1;
+            while (startDate.isBefore(endDate)) {
+                // periods.push(startDate.format('YYYY-[W]WW'));
+                periods.push('Minggu ' + weekNums);
+                startDate.add(1, 'week');
+                weekNums++;
+            }
+        } else {
+            throw new Error('Invalid filter');
+        }
+
+        const [rows] = await db.execute(sql, queryParams);
+
+        const scanData = [];
+        rows.forEach((item) => {
+            scanData[item.period] = item.scan_count;
+        });
+
+        let labels, data;
+
+        if (filter_by === 'month') {
+            labels = periods.map((period) =>
+                moment(period, 'YYYY-MM').format('MMMM')
+            );
+            data = periods.map((period) => scanData[period] || 0);
+        } else if (filter_by === 'week') {
+            labels = periods;
+            data = periods.map((_, index) => {
+                const periodKey = rows[index]?.period || `2024-W${index + 1}`;
+                return scanData[periodKey] || 0;
+            });
+        } else {
+            labels = periods;
+            data = periods.map((period) => scanData[period] || 0);
+        }
+
+        const response = {
+            labels,
+            datasets: [
+                {
+                    label: `Scans per ${
+                        filter_by.charAt(0).toUpperCase() + filter_by.slice(1)
+                    }`,
+                    data: data,
+                    borderColor:
+                        filter_by === 'month'
+                            ? 'rgb(255, 99, 132)'
+                            : filter_by === 'day'
+                            ? 'rgb(53, 162, 235)'
+                            : 'rgb(75, 192, 192)',
+                    backgroundColor:
+                        filter_by === 'month'
+                            ? 'rgba(255, 99, 132, 0.5)'
+                            : filter_by === 'day'
+                            ? 'rgba(53, 162, 235, 0.5)'
+                            : 'rgba(75, 192, 192, 0.5)',
+                },
+            ],
+        };
+
+        return httpResponse(res, httpStatus.OK, 'get stats', response);
+    } catch (error) {
+        return serverErrorResponse(res, error);
+    }
+};
+
 module.exports = {
     inputScan,
     getLastScanningQr,
     resetLastScanningQR,
+    getStatistics,
 };
--- src/routes/canteenScaneRoutes.js
+++ src/routes/canteenScaneRoutes.js
@@ -4,5 +4,6 @@ const CanteenScan = require('../controllers/canteenScanController');
 router.post('/input-scan', CanteenScan.inputScan);
 router.get('/last-scanning-qr', CanteenScan.getLastScanningQr);
 router.delete('/reset-last-scanning-qr', CanteenScan.resetLastScanningQR);
+router.get('/get-stats', CanteenScan.getStatistics);
 
 module.exports = router;
