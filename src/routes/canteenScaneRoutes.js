fdf44d490e0bd661735a7aab3f27b40b02e966c1
rxmy43 <ramyabyyu43@gmail.com>
2024-09-16 16:19:07 +0700

    fix get last scanning qr, and get stats endpoint access

--- src/routes/canteenScaneRoutes.js
+++ src/routes/canteenScaneRoutes.js
@@ -2,16 +2,22 @@ const router = require('express').Router();
 const CanteenScan = require('../controllers/canteenScanController');
 const auth = require('../middlewares/auth');
 const checkRole = require('../middlewares/checkRole');
+const { ROLES } = require('../utils/constants');
 
 router.post('/input-scan', auth, checkRole('Admin'), CanteenScan.inputScan);
 router.get(
     '/last-scanning-qr',
     auth,
-    checkRole('Admin'),
+    checkRole([ROLES.ADMIN, ROLES.BAK]),
     CanteenScan.getLastScanningQr
 );
 router.delete('/reset-last-scanning-qr', CanteenScan.resetLastScanningQR);
-router.get('/get-stats', auth, checkRole('Admin'), CanteenScan.getStatistics);
+router.get(
+    '/get-stats',
+    auth,
+    checkRole([ROLES.ADMIN, ROLES.BAK]),
+    CanteenScan.getStatistics
+);
 router.get('/export', auth, checkRole('Admin'), CanteenScan.exportCanteenScan);
 router.get(
     '/get-scanned',
